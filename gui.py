a69db9c78d4ca0359f1e9869ce6522449ad11137
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-10-30 22:03:59 +0600

    start a new for gui

--- gui.py
+++ gui.py
@@ -3,24 +3,23 @@ import customtkinter
 import webbrowser
 import main
 
-
-def button_callback():
-    label = customtkinter.CTkLabel(app, corner_radius=20, text="Button Pressed")
-    label.grid(row=1, column=2)
-
-def bookDataShow():
-    ...
+app = customtkinter.CTk()
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 def web_open(book):
     url = main.link_return(book)
     webbrowser.open(url, new=2)
 
-app = customtkinter.CTk()
-app.geometry("1920x1080")
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
-button = customtkinter.CTkButton(app, text="my button", command=button_callback)
-button.grid(row=1, column=1)
-button2 = tkinter.Button(app, text="Hello")
-button2.grid(row=1,column=2)
+def segmented_button_callback(value):
+    print("segmented button clicked:", value)
+
+segemented_button_var = customtkinter.StringVar(value=None)
+segemented_button = customtkinter.CTkSegmentedButton(app, values=["Value 1", "Value 2", "Value 3"],
+                                                    command=segmented_button_callback, dynamic_resizing=False,
+                                                    width=1920,height=30, variable=segemented_button_var
+                                                    )
+segemented_button.pack()
+
+app.after(0, lambda:app.state('zoomed')) # Don't know how this works but this put file in full screen mode
 app.mainloop()
