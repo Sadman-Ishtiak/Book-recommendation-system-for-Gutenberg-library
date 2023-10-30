ff8ccce1bf69a1f16f018f589b028df4b7e36595
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-10-30 20:13:05 +0600

    update on work

--- gui.py
+++ gui.py
@@ -1,16 +1,21 @@
 import tkinter
 import customtkinter
+import main
 
 
 def button_callback():
     label = customtkinter.CTkLabel(app, corner_radius=20, text="Button Pressed")
     label.grid(row=1, column=2)
 
+def bookDataShow():
+    ...
+
 app = customtkinter.CTk()
 app.geometry("1920x1080")
-customtkinter.set_appearance_mode("System")
+customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 button = customtkinter.CTkButton(app, text="my button", command=button_callback)
 button.grid(row=1, column=1)
-
+button2 = tkinter.Button(app, text="Hello")
+button2.grid(row=1,column=2)
 app.mainloop()
--- main.py
+++ main.py
@@ -3,17 +3,24 @@ import csv
 import random
 import sys
 
-# Global Variables
-books = []
+# Global Variables - Static prefarably
+_books_ = []
+_my_books_ = []
+# Global Variables - Changing type 
 retrun_query = []
-readed_book = []
+
+# caching list user has read
+with open("files\my_books.csv", "r", encoding="utf-8") as meh:
+    catalog = csv.DictReader(meh)
+    for i in catalog:
+        _my_books_.append(i)
 
 # reading and chaching the file
 with open("files\pg_catalog.csv",'r',encoding='utf-8') as csvfile:
     catalog = csv.DictReader(csvfile)
     for line in catalog:
         if line["Language"] == 'en' and line["Type"] == 'Text':
-            books.append(line)
+            _books_.append(line)
 
 # Defining the main function so that gui code can be added later
 def main():
@@ -29,22 +36,25 @@ def main():
         if flag == 0: break
         elif flag == 1:
             while True:
-                book = rando_recommend(books)
+                book = rando_recommend(_books_)
                 print_Book_CLI(book)
                 print("Do you want to read this book? (y/n) : ", end="")
-                ran = input()
-                if ran == "y" or ran == "Y":
+                usr_input = input()
+                if usr_input == "y" or usr_input == "Y":
                     print("Please copy the link and use a browser to get the book : ", link_return(book))
                     print("Saved to file")
                     sys.exit()
-                elif ran == "N" or ran == "n":
+                elif usr_input == "N" or usr_input == "n":
                     print("Another Random choice ?(y/n) : ", end="")
-                    ran = input()
-                    if ran == "n" or ran == "N" : break
+                    usr_input = input()
+                    if usr_input == "n" or usr_input == "N" : break
+        elif flag == 3 : 
+            print_list_cli(_my_books_)
         else : break
 
 def print_Book_CLI(a):
-    print(f"Book name     : {a['Title']}")
+    x = "\n"
+    print(f"Book name     : {a['Title'].replace(x, ' - ')}")
     print(f"Issue Date    : {a['Issued']}")
     print(f"Author's name : {a['Authors']}")
     print(f"Subjects      : {a['Subjects']}")
@@ -53,18 +63,43 @@ def print_Book_CLI(a):
 def rando_recommend(list):
     return random.choice(list)
 
+# Prints on the command line interface 
+def print_list_cli(list_to_print):
+    counter = 1
+    for i in list_to_print : 
+        name = i["Title"].replace("\n", "  -  ")
+        print(counter, ".", name)
+        counter += 1
+
+# Add to the read file for user 
+def add_book_to_read(book):
+    _my_books_.append(book)
+    try:
+        _books_.pop(_books_.index(book))
+    except: ...
+    with open("files\my_books.csv", "a") as fp:
+        writing = csv.DictWriter(fp)
+        writing.writerow(book)
+
 # This returns a list of books that have the same name as the author the user have put in
 def author_query():
     ...
 
 # This function is takes a string as input and returns a priority dictionary list
-def book_search():
+def book_search(Book_library, key):
+    cache_result = {}
+    for i in Book_library:
+        ...
+    return converted_to_list(cache_result)
+
+# This converts Priority map to list
+def converted_to_list(priority_dictionary):
     ...
 
 # Link return for the book
 def link_return(book):
     return "https://gutenberg.org/ebooks/" + str(book["Text"])
 
-# Calling the main function if and only if the file opened is this one
+# Calling the main function if and only if this file opened is this one
 if __name__ == "__main__":
     main()
