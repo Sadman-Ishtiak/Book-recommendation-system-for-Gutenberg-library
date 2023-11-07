96f9c0578338e6d87abfdb59bd9e83d2475e8457
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-11-07 17:18:58 +0600

    Update gui.py

--- gui.py
+++ gui.py
@@ -33,7 +33,7 @@ def segmented_button_callback(value):
         
 
 segemented_button_var = customtkinter.StringVar(value=None)
-segemented_button = customtkinter.CTkSegmentedButton(app, values=["Search", "List", "Recommend"], width=1000, height=30,
+segemented_button = customtkinter.CTkSegmentedButton(app, values=["Search", "List", "Recommend"], width=1000, height=35,
                                                     command=segmented_button_callback, dynamic_resizing=False,
                                                     corner_radius=10,
                                                     variable=segemented_button_var
@@ -44,16 +44,19 @@ frame_search = customtkinter.CTkFrame(app, width=1920, height=900, corner_radius
 frame_list = customtkinter.CTkFrame(app, width=1920, height=900, corner_radius=30)
 frame_recommend = customtkinter.CTkFrame(app, width=1920, height=900, corner_radius=30)
 
-lab1 = customtkinter.CTkLabel(frame_search, text="SEARCH")
 lab2 = customtkinter.CTkLabel(frame_list, text="List")
 lab3 = customtkinter.CTkLabel(frame_recommend, text="Recommend")
 lab4 = customtkinter.CTkLabel(frame_recommend, text="not Recommend")
-lab1.grid(row=1, column=1)
 lab4.grid(row=2, column=2)
 lab2.grid(row=1, column=1)
 lab3.grid(row=1, column=1)
 
 
+# Search frame 
+sbar = customtkinter.CTkEntry(frame_search, placeholder_text="Enter text", width=800)
+sbar.grid(row=1, column=1)
+s_button = customtkinter.CTkButton(frame_search, text="Search", width=200)
+s_button.grid(row=1, column=2)
 
 
 
