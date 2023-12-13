ef1a288e0d070234bb7cd3be02edcb099f1ca601
Sadman-Ishtiak <sadmanishtiak1@gmail.com>
2023-12-13 20:38:39 +0600

    Changes to be committed: 	modified:   gui.py

--- gui.py
+++ gui.py
@@ -13,9 +13,9 @@ customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 # The working functions 
-def web_open(book):
-    url = main.link_return(book)
-    webbrowser.open(url, new=2)
+# def web_open(book):
+#     url = main.link_return(book)
+#     webbrowser.open(url, new=2)
 
 def segmented_button_callback(value):
     if value == "Search":
